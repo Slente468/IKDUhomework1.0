0e9e24f4f219216b0a4017d6918830576c7a6201
Slente468 <GL51HW@student.aau.dk>
2022-10-27 15:41:02 +0200

    I tried

---
+++ IntroUnityImphenzia/Assets/Scripts/Player.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private Transform groundCheckTransform = null;
+    [SerializeField] private LayerMask playerMask;
+    [SerializeField] private float movmentSpeed = 5;
+    //bool is checking whether it's true or false
+    // also yes and no
+    //true is 1 
+    // false is 0
+    private bool jumpKeyWasPressed;
+    private float horizontalInput;
+
+    
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // tjek om space er presset ned
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("space is pressed down");
+            jumpKeyWasPressed = true;
+
+            // 
+
+            horizontalInput = Input.GetAxis("Horizontal");
+        }
+        
+
+    }
+
+    // FixedUpdate is called every time some physics updates 
+    private void FixedUpdate()
+    {
+        GetComponent<Rigidbody>().velocity = new Vector3(horizontalInput*movmentSpeed, GetComponent<Rigidbody>().velocity.y, 0);
+
+        if (Physics.OverlapSphere(groundCheckTransform.position, 0.1f, playerMask).Length == 1)
+       
+
+        if(jumpKeyWasPressed)
+
+        GetComponent<Rigidbody>().AddForce(Vector3.up * 6, ForceMode.VelocityChange);
+        if (jumpKeyWasPressed)
+            jumpKeyWasPressed = false;
+       
+
+    }
+  
+}
